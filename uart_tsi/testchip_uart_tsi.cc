a3fc9a17b4de2c23c78ee4483c361ef907f9fd15
Jim Fang <yf328@berkeley.edu>
2025-05-27 04:20:29 -0700

    make apple compatible: do not use baud rates unsupported by macos

--- uart_tsi/testchip_uart_tsi.cc
+++ uart_tsi/testchip_uart_tsi.cc
@@ -9,6 +9,49 @@
 
 #define PRINTF(...) printf("UART-TSI: " __VA_ARGS__);
 
+/* ------------------------------------------------------------------ */
+/* Darwin / *BSD don’t define the high-speed termios constants that   */
+/* glibc does.  On those systems cfsetispeed/cfsetospeed accept *any* */
+/* positive integer, so we can safely map the symbolic names to their */
+/* numeric baud‐rate values.                                          */
+/* ------------------------------------------------------------------ */
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__)
+#ifndef B460800
+#define B460800 460800
+#endif
+#ifndef B500000
+#define B500000 500000
+#endif
+#ifndef B576000
+#define B576000 576000
+#endif
+#ifndef B921600
+#define B921600 921600
+#endif
+#ifndef B1000000
+#define B1000000 1000000
+#endif
+#ifndef B1152000
+#define B1152000 1152000
+#endif
+#ifndef B1500000
+#define B1500000 1500000
+#endif
+#ifndef B2000000
+#define B2000000 2000000
+#endif
+#ifndef B2500000
+#define B2500000 2500000
+#endif
+#ifndef B3000000
+#define B3000000 3000000
+#endif
+#ifndef B4000000
+#define B4000000 4000000
+#endif
+#endif /* __APPLE__/BSD */
+/* ------------------------------------------------------------------ */
+
 testchip_uart_tsi_t::testchip_uart_tsi_t(int argc, char** argv,
 					 char* ttyfile, uint64_t baud_rate,
 					 bool verbose, bool do_self_check)
